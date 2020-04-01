83541ba6f1cba77706f00719e7f4c084dbfff4e7
김광현 <greentea@zuminternet.com>
2020-04-01 10:30:55 +0900

    feat : poll init test

--- src/test/java/ladder/PollTest.java
+++ src/test/java/ladder/PollTest.java
@@ -1,9 +1,29 @@
 package ladder;
 
-import org.junit.jupiter.api.Test;
+import ladder.strategy.PollCreateStrategy;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PollTest {
 
+    @ParameterizedTest
+    @MethodSource
+    void initTest(PollCreateStrategy pollCreateStrategy) {
+        Poll poll = Poll.init(pollCreateStrategy);
+
+        assertThat(poll).isNotNull();
+        assertThat(poll.isExist()).isEqualTo(pollCreateStrategy.returnCreateOrNo());
+    }
+
+    private static Stream initTest() {
+        return Stream.of(
+                Arguments.of((PollCreateStrategy) () -> true),
+                Arguments.of((PollCreateStrategy)() -> false)
+        );
+    }
 }
