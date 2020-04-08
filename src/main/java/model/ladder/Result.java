9e6ce3c59f9a42f4715296c9e7d076ba50a4eefd
PARK SUNG DONG <oraedoa@gmail.com>
2020-04-08 23:30:57 +0900

    [style] refactor typo error

--- src/main/java/model/ladder/Result.java
+++ src/main/java/model/ladder/Result.java
@@ -1,12 +1,15 @@
 package model.ladder;
 
+import exception.LadderResultCreateException;
+import org.apache.commons.lang3.StringUtils;
+
 public class Result {
 
-    private String winnerPrice;
+    private int winnerPrice;
     private ResultType type;
 
     private enum ResultType{
-        MONNEY, END;
+        MONEY, END;
     }
 
     public Result(String result) {
@@ -20,7 +23,9 @@ public class Result {
         type = ResultType.getMyType(result);
     }
 
-    private void validate() {
-
+    private void validate(String result) {
+        if(StringUtils.isEmpty(result)) {
+            throw new LadderResultCreateException("사다리의 결과가 없기 때문에 사다리를 만들 수 없습니다.");
+        }
     }
 }
