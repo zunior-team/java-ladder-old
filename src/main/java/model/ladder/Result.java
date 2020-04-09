db47ef7f746b3e3ebc395d2fd97c732022a04590
PARK SUNG DONG <oraedoa@gmail.com>
2020-04-09 17:30:11 +0900

    [test & feat] add score-type Enum class

--- src/main/java/model/ladder/Result.java
+++ src/main/java/model/ladder/Result.java
@@ -5,27 +5,36 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Result {
 
-    private int winnerPrice;
+    private int score;
     private ResultType type;
 
     private enum ResultType{
-        MONEY, END;
+        POINT, END;
     }
 
     public Result(String result) {
         validate(result);
 
         try{
-            winnerPrice = Integer.parseInt(result);
-        } catch(NumberFormatException) {
-
+            score = Integer.parseInt(result);
+            type = ResultType.POINT;
+        } catch(NumberFormatException e) {
+            score = 0;
+            type = ResultType.END;
         }
-        type = ResultType.getMyType(result);
+
+        validateNegative();
     }
 
     private void validate(String result) {
         if(StringUtils.isEmpty(result)) {
             throw new LadderResultCreateException("사다리의 결과가 없기 때문에 사다리를 만들 수 없습니다.");
+        }
+    }
+
+    private void validateNegative(){
+        if(score < 0){
+            throw new LadderResultCreateException("들어온 점수가 음수이기 때문에 사다리를 만들 수 없습니다.");
         }
     }
 }
---
+++ src/main/java/model/ladder/Score.java
@@ -0,0 +1,7 @@
+package model.ladder;
+
+public class Score {
+
+    
+
+}
