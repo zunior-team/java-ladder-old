a96ae181223686cd63e59f2244e18b1ff0db2ee0
최준우 <junwoochoi@zuminternet.com>
2020-04-27 08:30:19 +0900

    typo : fix typo

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -1,21 +1,17 @@
 package ladder.domain;
 
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@DisplayName("라인 객체 테스")
+@DisplayName("라인 객체 테스트")
 class LineTest {
 
     @Test
