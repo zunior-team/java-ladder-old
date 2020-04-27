bc8694488feac60e32f252a010363cccec1dedfd
최준우 <junwoochoi@zuminternet.com>
2020-04-27 09:08:35 +0900

    refactor : 불필요한 의존성 제거

--- src/main/java/ladder/domain/Results.java
+++ src/main/java/ladder/domain/Results.java
@@ -4,24 +4,22 @@ import spark.utils.CollectionUtils;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 public class Results {
-    private List<String> results;
+    private final List<String> results;
 
-    private Results(List<String> resultNames, Players players) {
-        Objects.requireNonNull(players, "참여자 값이 존재하지 않습니다.");
+    private Results(List<String> resultNames, int playerCount) {
         if (CollectionUtils.isEmpty(resultNames)) {
             throw new IllegalArgumentException("결과 값은 비어 있을 수 없습니다.");
         }
-        if (resultNames.size() != players.size()) {
+        if (resultNames.size() != playerCount) {
             throw new IllegalArgumentException("결과 갯수가 참여자 수와 다릅니다.");
         }
         this.results = Collections.unmodifiableList(resultNames);
     }
 
-    public static Results of(List<String> resultNames, Players players) {
-        return new Results(resultNames, players);
+    public static Results of(List<String> resultNames, int playerCount) {
+        return new Results(resultNames, playerCount);
     }
 
     public String findByIndex(int index) {
